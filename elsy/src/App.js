5d27524b67cbda5073f44ffcbd103c654affd718
anitamayousse <anita.mayousse@gmail.com>
2022-01-24 15:23:13 +0100

    step02

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <p>
+          Heart : {heartMin}
+          Temperature : {tempMin}
+          Steps : {stepsMin}
+          </p>
+        </div>
+        <h1 className="text-primary">Hello !</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
